8c0518fe1a099d710f8c9d6f5f70cebd3ca17c98
nuric <nuric@users.noreply.github.com>
2020-06-09 11:32:14 +0100

    Format dummy training script.

--- train.py
+++ train.py
@@ -5,22 +5,22 @@ import gym
 # Disable scientific printing
 np.set_printoptions(threshold=10000, suppress=True, precision=5, linewidth=180)
 
-env = gym.make('CartPole-v1')
+env = gym.make("CartPole-v1")
 print("Observation:")
 print(env.observation_space)
 print("Action:")
 print(env.action_space)
 
 for k in range(2):
-  print("\n-------\n")
-  observation: np.ndarray = env.reset()
-  for t in range(100):
-    # env.render()
-    print(observation)
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
-    if done:
-      print(f"Episode finished after {t+1} timesteps")
-      print("\n-------\n")
-      break
+    print("\n-------\n")
+    observation: np.ndarray = env.reset()
+    for t in range(100):
+        # env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print(f"Episode finished after {t+1} timesteps")
+            print("\n-------\n")
+            break
 env.close()
