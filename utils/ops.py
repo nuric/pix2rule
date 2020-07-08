6f5624e024c18277b113d67272543313e148f768
nuric <nuric@users.noreply.github.com>
2020-07-08 11:34:55 +0100

    Refactor utility operations into utils.ops

---
+++ utils/ops.py
@@ -0,0 +1,19 @@
+"""Utility operations."""
+import tensorflow as tf
+
+
+def leftright_cumprod(tensor: tf.Tensor) -> tf.Tensor:
+    """Convert sigmoid outputs to categorical distribution."""
+    # tensor (..., X)
+    cumprod = tf.math.cumprod(1 - tensor, axis=-1, exclusive=True)  # (..., X)
+    return tensor * cumprod  # (..., X)
+
+
+def reduce_probsum(
+    tensor: tf.Tensor, axis: int = -1, keepdims: bool = False
+) -> tf.Tensor:
+    """Reduce given axis using probabilistic sum."""
+    # tensor (..., X, ...)
+    return 1 - tf.reduce_prod(
+        1 - tensor, axis=axis, keepdims=keepdims
+    )  # (..., 1?, ...)
