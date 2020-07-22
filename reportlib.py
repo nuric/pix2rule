ad855f4f7395200aff0e297d514b897523025009
nuric <nuric@users.noreply.github.com>
2020-07-22 12:43:12 +0100

    Add reportlib for global model debugging and tensor reporting.

---
+++ reportlib.py
@@ -0,0 +1,26 @@
+"""Global reporting tool to extract eager tensors."""
+from typing import Dict, Any
+import pdb
+
+import tensorflow as tf
+
+report: Dict[str, Any] = dict()
+
+
+def create_report(model: tf.keras.Model, dataset: tf.data.Dataset) -> Dict[str, Any]:
+    """Take 1 batch from dataset and perform a forward pass of model."""
+    report.clear()
+    report["debug"] = True
+    for inputs, labels in dataset.take(1):
+        # Models should populate global report dictionary
+        report.update(inputs)
+        report["labels"] = labels
+        report["outputs"] = model(inputs)
+    report["debug"] = False
+    return {k: v.numpy() for k, v in report.items() if k != "debug"}
+
+
+def report_break():
+    """Setup reporting breakpoint."""
+    if report.get("debug", False):
+        pdb.set_trace()
