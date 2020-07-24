2732a20eea854b70a32dc3f01f7005265396e049
nuric <nuric@users.noreply.github.com>
2020-07-24 14:40:29 +0100

    Add configuration hash as tag to mlflow runs.

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 """Unification MLP."""
+from typing import Dict, Any
 import os
 import logging
 import datetime
 from pathlib import Path
 import json
+import hashlib
 
 import numpy as np
 import tensorflow as tf
@@ -31,7 +33,6 @@ np.set_printoptions(suppress=True, precision=5, linewidth=180)
 # Arguments
 parser = configlib.add_parser("UMLP options.")
 parser.add_argument("--experiment_name", help="Optional experiment name.")
-parser.add_argument("--run_name", default="main", help="Optional experiment name.")
 parser.add_argument(
     "--invariants", default=1, type=int, help="Number of invariants per task."
 )
@@ -97,6 +98,16 @@ def train():
     logging.info("Training completed.")
 
 
+def dict_hash(dictionary: Dict[str, Any]) -> str:
+    """MD5 hash of a dictionary."""
+    dhash = hashlib.md5()
+    # We need to sort arguments so {'a': 1, 'b': 2} is
+    # the same as {'b': 2, 'a': 1}
+    encoded = json.dumps(dictionary, sort_keys=True).encode()
+    dhash.update(encoded)
+    return dhash.hexdigest()
+
+
 def main():
     """Main entry point function."""
     # ---------------------------
@@ -117,20 +128,39 @@ def main():
     mlflow.set_experiment(C["experiment_name"])
     logger.info("Set experiment %s", C["experiment_name"])
     # ---
-    art_dir = Path(C["experiment_name"]) / C["run_name"]
+    # Check for past run, are we resuming?
+    config_hash = dict_hash(C)
+    logger.info("Configuration hash is %s", config_hash)
+    run_id = None
+    past_runs = mlflow.search_runs(
+        filter_string=f"tags.config_hash = '{config_hash}'", max_results=1
+    )
+    if not past_runs.empty:
+        # run_id = past_runs["run_id"][0]
+        logger.info("Resuming run with id %s", run_id)
+        raise NotImplementedError("Resuming runs in the same experiment.")
+    # ---
+    # Setup mlflow tracking
+    mlflow_run = mlflow.start_run(run_id=run_id)
+    run_id = mlflow_run.info.run_id  # either brand new or the existing one
+    logger.info("Experiment id: %s", mlflow_run.info.experiment_id)
+    logger.info("Run id: %s", run_id)
+    mlflow.log_params(C)
+    mlflow.set_tag("config_hash", config_hash)
+    art_dir = Path(C["experiment_name"]) / run_id
     art_dir.mkdir(parents=True, exist_ok=True)
     C["artifact_dir"] = str(art_dir)
+    logger.info("Local artifact dir is %s", str(art_dir))
+    logger.info("Artifact uri is %s", mlflow.get_artifact_uri())
+    # ---
     # ---------------------------
     # Big data machine learning in the cloud
     try:
-        mlflow_run = mlflow.start_run()
-        logger.info("Experiment id: %s", mlflow_run.info.experiment_id)
-        logger.info("Run id: %s", mlflow_run.info.run_id)
-        mlflow.log_params(C)
-        mlflow.set_tag("config_hash", hash(json.dumps(C, sort_keys=True)))
-        logger.info("Artifact uri is %s", mlflow.get_artifact_uri())
         train()
     except KeyboardInterrupt:
+        logger.warning("Pausing training on keyboard interrupt.")
+    except Exception as e:
+        logger.error("Terminating training: %s", e)
         mlflow.end_run(status="KILLED")
     else:
         mlflow.end_run()
