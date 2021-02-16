a525eb145d1804d410d8c9d8fe3ca6e98f7b8bd8
nuric <nuric@users.noreply.github.com>
2021-02-16 16:41:57 +0000

    Fix unknown dimension in spacial flatten.

--- components/spacial_flatten.py
+++ components/spacial_flatten.py
@@ -8,6 +8,11 @@ class SpacialFlatten(tf.keras.layers.Layer):
     def call(self, inputs: tf.Tensor, **kwargs):
         """Flatten except batch and last dimension."""
         # tensor (B, ..., X)
-        shape = tf.shape(inputs)  # [B, ..., X]
-        new_shape = tf.concat([shape[0], -1, shape[-1]], 0)  # [B, -1, X]
+        batch_size = tf.shape(inputs)[0]  # B
+        # We take this differently as above method gives None at compile time
+        # which makes upstream layers unhappy. But it safe to assume the number
+        # of features is known before hand.
+        features_size = inputs.shape[-1]  # X
+        assert features_size is not None, "Last dimension to spacial flatten is None."
+        new_shape = tf.stack([batch_size, -1, features_size], 0)  # [B, -1, X]
         return tf.reshape(inputs, new_shape)  # (B, ., X)
