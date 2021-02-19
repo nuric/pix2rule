46d368d4c978d7863d3ff5936b64728b2df7fb5a
nuric <nuric@users.noreply.github.com>
2021-02-19 11:39:23 +0000

    Refactor hyperrun and add chaining.

--- utils/hyperrun.py
+++ utils/hyperrun.py
@@ -1,19 +1,31 @@
 """Script to run experiments."""
-from typing import Dict, Any, Iterator
+from typing import Dict, Any, List
 import itertools
 from . import hashing
 
 
-def generate_configs(
-    hconfig: Dict[str, Any], with_hash=False
-) -> Iterator[Dict[str, Any]]:
+def generate_configs(hconfig: Dict[str, Any], with_hash=False) -> List[Dict[str, Any]]:
     """Generate every config from lists in a dictionary."""
     lists = {k: v for k, v in hconfig.items() if isinstance(v, list)}
     notlists = {k: v for k, v in hconfig.items() if not isinstance(v, list)}
     # Compute cross product between list entry values
+    configs: List[Dict[str, Any]] = list()
     for instance in itertools.product(*lists.values()):
         config_dict = dict(zip(lists.keys(), instance))
         config_dict.update(notlists)
         if with_hash:
             config_dict["hash"] = hashing.dict_hash(config_dict)
-        yield config_dict
+        configs.append(config_dict)
+    return configs
+
+
+def chain_configs(*configs: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Chain given iterable config dictionaries."""
+    chained_configs: List[Dict[str, Any]] = list()
+    for instance in itertools.product(*configs):
+        merged_config: Dict[str, Any] = dict()
+        # instance (dict_a, dict_b, ...)
+        for instance_dict in instance:
+            merged_config.update(instance_dict)
+        chained_configs.append(merged_config)
+    return chained_configs
