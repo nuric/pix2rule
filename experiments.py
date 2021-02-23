789d6a1743f9c6a9992e9a016a7a0461c6673b2e
nuric <nuric@users.noreply.github.com>
2021-02-23 14:44:34 +0000

    Setup base experiments.

--- experiments.py
+++ experiments.py
@@ -19,6 +19,7 @@ import datasets
 # Parse configuration parameters
 configlib.parse()
 C = configlib.config
+configlib.save_config(str(Path(C["data_dir"]) / "defaults.json"))
 print(f"There are {len(C.keys())} many configuration parameters.")
 # ---------------------------
 
@@ -26,10 +27,10 @@ all_experiments: List[Dict[str, Any]] = list()
 # ---------------------------
 # Relsgame dataset with different training sizes
 relsgame_exp = {
-    "experiment_name": "relsgame_full",
+    "experiment_name": "relsgame_1k_full",
     "max_steps": 30000,
     "eval_every": 200,  # divide max_steps by this to get epochs in keras
-    "learning_rate": [0.001],
+    "learning_rate": 0.001,
     "dataset_name": "relsgame",
     "relsgame_tasks": [
         ["same"],
@@ -39,16 +40,41 @@ relsgame_exp = {
         ["colour_and_or_shape"],
         [],
     ],
-    "relsgame_train_size": [10000, 1000, 100, 10],
+    "relsgame_train_size": 1000,
     "relsgame_validation_size": 1000,
     "relsgame_test_size": 1000,
     "relsgame_batch_size": 64,
 }
-relsgame_models = [
+relsgame_models: List[Dict[str, Any]] = [
     {
         "model_name": "dnf_image_classifier",
+        "dnf_image_layer_name": "RelationsGameImageInput",
+        "dnf_image_hidden_size": 32,
+        "dnf_image_activation": "relu",
+        "dnf_image_noise_stddev": 0.0,
+        "dnf_image_with_position": True,
+        "dnf_object_sel_layer_name": "RelaxedObjectSelection",
+        "dnf_object_sel_num_select": 4,
+        "dnf_object_sel_initial_temperature": 0.5,
+        "dnf_object_feat_layer_name": "LinearObjectFeatures",
+        "dnf_object_feat_unary_size": 6,
+        "dnf_object_feat_binary_size": 12,
+        "dnf_hidden_predicates": [],
+        "dnf_iterations": 2,
         "relsgame_one_hot_labels": True,
-    }
+    },
+    {
+        "model_name": "predinet",
+        "predinet_image_layer_name": "RelationsGameImageInput",
+        "predinet_image_hidden_size": 32,
+        "predinet_image_activation": "relu",
+        "predinet_image_noise_stddev": 0.0,
+        "predinet_image_with_position": True,
+        "predinet_relations": 12,
+        "predinet_heads": 8,
+        "predinet_key_size": 32,
+        "predinet_output_hidden_size": 64,
+    },
 ]
 relsgame_exps = hp.chain(hp.generate(relsgame_exp), relsgame_models)
 all_experiments.extend(relsgame_exps)
