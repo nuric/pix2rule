38750d50786b55e486415cb9e0cf1422fdfad7d0
nuric <nuric@users.noreply.github.com>
2021-03-02 15:10:06 +0000

    Add wip real dnf layer for unbounded reasoning

--- components/dnf_layer.py
+++ components/dnf_layer.py
@@ -14,14 +14,14 @@ import itertools
 import numpy as np
 import tensorflow as tf
 
-from components.ops import reduce_probsum, flatten_concat
+from components.ops import reduce_probsum, flatten_concat, soft_maximum, soft_minimum
 from components.initialisers import CategoricalRandomNormal, BernoulliRandomNormal
 
 configurable: Dict[str, Dict[str, Any]] = {
     "layer_name": {
         "type": str,
-        "default": "DNFLayer",
-        "choices": ["DNFLayer"],
+        "default": "DNF",
+        "choices": ["DNF", "RealDNF"],
         "help": "DNF layer to use.",
     },
     "arities": {
@@ -48,7 +48,7 @@ configurable: Dict[str, Dict[str, Any]] = {
 }
 
 
-class DNFLayer(tf.keras.layers.Layer):  # pylint: disable=too-many-instance-attributes
+class DNF(tf.keras.layers.Layer):  # pylint: disable=too-many-instance-attributes
     """Single layer that represents conjunction with permutation invariance."""
 
     def __init__(
@@ -82,6 +82,17 @@ class DNFLayer(tf.keras.layers.Layer):  # pylint: disable=too-many-instance-attr
             initializer=tf.keras.initializers.Constant(1),
             trainable=False,
         )
+        # ---------------------------
+        # Indices for THERE EXISTS operations if applicable
+        try:
+            binary_index = self.arities.index(2)
+        except ValueError:
+            binary_index = len(self.arities)
+        try:
+            unary_index = self.arities.index(1)
+        except ValueError:
+            unary_index = binary_index
+        self.rule_idxs = (unary_index, binary_index)
 
     def build(self, input_shape: Dict[str, tf.TensorShape]):
         """Build layer weights."""
@@ -228,26 +239,17 @@ class DNFLayer(tf.keras.layers.Layer):  # pylint: disable=too-many-instance-attr
         rule_shape = tf.concat([eval_shape[:1], object_perms, eval_shape[2:]], 0)
         rules = tf.reshape(disjuncts, rule_shape)  # (B, N, N-1, ..., R)
         # ---------------------------
-        # THERE EXISTS operations if applicable
-        try:
-            binary_index = self.arities.index(2)
-        except ValueError:
-            binary_index = len(self.arities)
-        try:
-            unary_index = self.arities.index(1)
-        except ValueError:
-            unary_index = binary_index
-        # ---
         # For nullary reduce over all variables, i.e. there exists XYZ ...
+        unary_index, binary_index = self.rule_idxs
         nullary_rules = rules[..., :unary_index]  # (B, N, N-1, ..., R0)
         var_range = tf.range(self.num_total_variables) + 1  # [1, 2, ...] x V
-        nullary_rules = tf.reduce_max(nullary_rules, var_range)  # (B, R0)
+        nullary_rules = reduce_probsum(nullary_rules, var_range)  # (B, R0)
         # For unary reduce over remaining variables, i.e. For all X, there exists YZ
         unary_rules = rules[..., unary_index:binary_index]  # (B, N, N-1, ..., R1)
-        unary_rules = tf.reduce_max(unary_rules, var_range[1:])  # (B, N, R1)
+        unary_rules = reduce_probsum(unary_rules, var_range[1:])  # (B, N, R1)
         # For binary reduce over remaining variables except first 2 and so on
         binary_rules = rules[..., binary_index:]  # (B, N, N-1, ..., R2)
-        binary_rules = tf.reduce_max(binary_rules, var_range[2:])  # (B, N, N-1, R2)
+        binary_rules = reduce_probsum(binary_rules, var_range[2:])  # (B, N, N-1, R2)
         outputs = {
             "nullary": nullary_rules,
             "unary": unary_rules,
@@ -269,10 +271,11 @@ class DNFLayer(tf.keras.layers.Layer):  # pylint: disable=too-many-instance-attr
                 if count == 0:
                     outputs[k] = inputs[k]
                     continue
-                old_value = inputs[k][..., -count:]  # (..., RX)
-                merged_value = tf.stack([old_value, outputs[k]], -1)  # (..., RX, 2)
+                # old_value = inputs[k][..., -count:]  # (..., RX)
+                # merged_value = tf.stack([old_value, outputs[k]], -1)  # (..., RX, 2)
                 # Amalgamate function here, we use probabilistic sum
-                next_value = reduce_probsum(merged_value, -1)  # (..., RX)
+                # next_value = reduce_probsum(merged_value, -1)  # (..., RX)
+                next_value = outputs[k]
                 outputs[k] = tf.concat([inputs[k][..., :-count], next_value], -1)
         return outputs
 
@@ -288,3 +291,133 @@ class DNFLayer(tf.keras.layers.Layer):  # pylint: disable=too-many-instance-attr
             }
         )
         return config
+
+
+class RealDNF(DNF):
+    """Real valued DNF layer."""
+
+    def call(self, inputs: Dict[str, tf.Tensor], **kwargs):
+        """Perform forward pass of the model."""
+        # pylint: disable=too-many-locals
+        # inputs {'nullary_preds': (B, P0), 'unary_preds': (B, N, P1),
+        #         'binary_preds': (B, N, N-1, P2)}
+        # ---------------------------
+        # Setup permutations
+        # Conjunctions in logic are permutation invariant A and B <=> B and A
+        # so we take naive approach of evaluating all permutations since there is
+        # no background knowledge / mode bias to restrict the search space.
+        perm_nullary = tf.repeat(
+            inputs["nullary"][:, None], self.perm_idxs.shape[0], axis=1
+        )  # (B, K, P0)
+        perm_unary = tf.gather(inputs["unary"], self.perm_idxs, axis=1)  # (B, K, V, P1)
+        repeat_bidxs = tf.repeat(
+            self.perm_bidxs[None], tf.shape(inputs["binary"])[0], axis=0
+        )  # (B, K, V, V-1, 2)
+        perm_binary = tf.gather_nd(
+            inputs["binary"], repeat_bidxs, batch_dims=1
+        )  # (B, K, V, V-1, P2)
+        # ---------------------------
+        # Compute flattened input
+        in_tensor = flatten_concat(
+            [perm_nullary, perm_unary, perm_binary], batch_dims=2
+        )
+        # (B, K, P0 + V*P1 + V*(V-1)*P2)
+        # ---------------------------
+        # Compute weighted conjunct truth values
+        and_kernel = tf.nn.softmax(
+            self.and_kernel / self.temperature, -1
+        )  # (R, H, IN, 3)
+        in_tensor = in_tensor[:, :, None, None]  # (B, K, 1, 1, IN)
+        conjuncts_eval = (
+            in_tensor * and_kernel[..., 0]
+            + (in_tensor * -1) * and_kernel[..., 1]
+            + and_kernel[..., 2]
+            * soft_maximum(in_tensor, temperature=self.temperature, keepdims=True)
+        )  # (B, K, R, H, IN)
+        # ---
+        # AND operation
+        conjuncts = soft_minimum(conjuncts_eval, axis=-1, temperature=self.temperature)
+        # (B, K, R, H)
+        # ---------------------------
+        # Compute weighted disjunct truth values
+        or_kernel = tf.nn.sigmoid(self.or_kernel / self.temperature)  # (R, H)
+        disjuncts = conjuncts * or_kernel + (1 - or_kernel) * soft_minimum(
+            conjuncts, temperature=self.temperature, keepdims=True
+        )
+        # (B, K, R, H)
+        # ---
+        # OR operation
+        disjuncts = soft_maximum(disjuncts, axis=-1, temperature=self.temperature)
+        # (B, K, R)
+        # ---------------------------
+        # Reduce conjunction
+        # We will reshape and reduce according to the arity of these conjuncts
+        num_objects = tf.shape(inputs["unary"])[1:2]  # [N]
+        # ---------------------------
+        # For nullary reduce over all variables, i.e. there exists XYZ ...
+        unary_index, binary_index = self.rule_idxs
+        nullary_rules = disjuncts[..., :unary_index]  # (B, K, R0)
+        nullary_rules = soft_maximum(
+            nullary_rules, axis=1, temperature=self.temperature
+        )  # (B, R0)
+        # For unary reduce over remaining variables, i.e. For all X, there exists YZ
+        unary_rules = disjuncts[..., unary_index:binary_index]  # (B, K, R1)
+        unary_shape = tf.shape(unary_rules)  # [B, K, R1]
+        unary_shape = tf.concat(
+            [
+                unary_shape[:1],
+                num_objects,
+                unary_shape[1:2] // num_objects,
+                unary_shape[2:],
+            ],
+            0,
+        )  # (B, N, K/N, R1)
+        unary_rules = tf.reshape(unary_rules, unary_shape)
+        unary_rules = soft_maximum(
+            unary_rules, axis=2, temperature=self.temperature
+        )  # (B, N, R1)
+        # For binary reduce over remaining variables except first 2 and so on
+        binary_rules = disjuncts[..., binary_index:]  # (B, K, R2)
+        binary_shape = tf.shape(binary_rules)  # [B, K, R2]
+        binary_shape = tf.concat(
+            [
+                binary_shape[:1],
+                num_objects,
+                num_objects - 1,
+                binary_shape[1:2] // (num_objects * (num_objects - 1)),
+                binary_shape[2:],
+            ],
+            0,
+        )  # (B, N, N-1, K/(N*(N-1)), R2)
+        binary_rules = tf.reshape(binary_rules, binary_shape)
+        binary_rules = soft_maximum(
+            binary_rules, axis=3, temperature=self.temperature
+        )  # (B, N, N-1, R2)
+        outputs = {
+            "nullary": nullary_rules,
+            "unary": unary_rules,
+            "binary": binary_rules,
+            "and_kernel": and_kernel,  # We return the kernels for analysis
+            "or_kernel": or_kernel,
+        }
+        # ---------------------------
+        # Merge or compute return values
+        if self.recursive:
+            keys = ["nullary", "unary", "binary"]
+            # We assume the last R predicates are the learnt rules, so we slice
+            # and concenate back their new values. i.e. amalgamate
+            for i, k in enumerate(keys):
+                count = self.arities.count(i)
+                # Check if we have any variables at hand
+                # tf.function should handle this if statement
+                # as it is a fixed pure Python check
+                if count == 0:
+                    outputs[k] = inputs[k]
+                    continue
+                # old_value = inputs[k][..., -count:]  # (..., RX)
+                # merged_value = tf.stack([old_value, outputs[k]], -1)  # (..., RX, 2)
+                # Amalgamate function here, we use probabilistic sum
+                # next_value = reduce_probsum(merged_value, -1)  # (..., RX)
+                next_value = outputs[k]
+                outputs[k] = tf.concat([inputs[k][..., :-count], next_value], -1)
+        return outputs
--- components/test_dnf_layer.py
+++ components/test_dnf_layer.py
@@ -22,7 +22,7 @@ class TestDNFLayer(tf.test.TestCase):
     def test_different_arities(self):
         """Returns correct number of rules."""
         facts = gen_random_facts()
-        facts = dnf_layer.DNFLayer(arities=[0, 1, 1, 2, 2, 2])(facts)
+        facts = dnf_layer.DNF(arities=[0, 1, 1, 2, 2, 2])(facts)
         self.assertEqual(facts["nullary"].shape, [4, 1])
         self.assertEqual(facts["unary"].shape, [4, 2, 2])
         self.assertEqual(facts["binary"].shape, [4, 2, 1, 3])
@@ -30,7 +30,7 @@ class TestDNFLayer(tf.test.TestCase):
     def test_recursive_merge(self):
         """When run recursively, it merges the learnt rules."""
         facts = gen_random_facts()
-        dnf = dnf_layer.DNFLayer(arities=[0, 1, 1, 2, 2, 2], recursive=True)
+        dnf = dnf_layer.DNF(arities=[0, 1, 1, 2, 2, 2], recursive=True)
         facts = dnf.pad_inputs(facts)
         facts = dnf(facts)
         self.assertEqual(facts["nullary"].shape, [4, 3])
@@ -40,5 +40,5 @@ class TestDNFLayer(tf.test.TestCase):
     def test_different_num_variables(self):
         """Works with different number of variables."""
         facts = gen_random_facts(4)
-        facts = dnf_layer.DNFLayer(arities=[2], num_total_variables=3)(facts)
+        facts = dnf_layer.DNF(arities=[2], num_total_variables=3)(facts)
         self.assertEqual(facts["binary"].shape, [4, 4, 3, 1])
