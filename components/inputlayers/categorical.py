8fe2b7311142e67443cb194a43ac1ace842e4498
nuric <nuric@users.noreply.github.com>
2021-03-11 18:18:43 +0000

    Add optional activation to categorical input layer

--- components/inputlayers/categorical.py
+++ components/inputlayers/categorical.py
@@ -5,21 +5,23 @@ import tensorflow as tf
 class OneHotCategoricalInput(tf.keras.layers.Layer):
     """Process a categorical input as one-hot."""
 
-    def __init__(self, num_categories: int, **kwargs):
+    def __init__(self, num_categories: int, activation: str = "sigmoid", **kwargs):
         super().__init__(**kwargs)
         self.num_categories = num_categories
+        self.activation = activation
 
     def call(self, inputs: tf.Tensor, **kwargs):
         """Forward pass which encodes inputs as one-hot."""
         # inputs (B,)
-        return tf.one_hot(inputs, self.num_categories, on_value=1.0, off_value=0.0)
+        off_value = -1.0 if self.activation == "tanh" else 0.0
+        return tf.one_hot(
+            inputs, self.num_categories, on_value=1.0, off_value=off_value
+        )
 
     def get_config(self):
         """Serialisable configuration dictionary."""
         config = super().get_config()
         config.update(
-            {
-                "num_categories": self.num_categories,
-            }
+            {"num_categories": self.num_categories, "activation": self.activation}
         )
         return config
