35f2c282dd03220667ba9b42ffdfc7358a9c80e6
nuric <nuric@users.noreply.github.com>
2021-03-30 16:00:15 +0100

    Refactor clingo utils into separate functions

--- utils/clingo.py
+++ utils/clingo.py
@@ -9,10 +9,29 @@ import numpy as np
 import tqdm
 
 
-def clingo_check(
+def run_clingo(logic_program: List[str], cleanup: bool = False) -> bool:
+    """Run clingo for satisfiability check for a given program."""
+    # Write the program file.
+    with open("clingo_temp.lp", "w") as fout:
+        fout.write("\n".join(logic_program))
+    # ---------------------------
+    # Ask clingo the result
+    # Clingo for some reason returns exit code 10 on success, we won't have return check
+    res = subprocess.run(
+        ["clingo", "clingo_temp.lp"], capture_output=True, check=False, text=True
+    )
+    # ---------------------------
+    # Clean up
+    if cleanup:
+        os.remove("clingo_temp.lp")
+    # ---------------------------
+    return "UNSATISFIABLE" not in res.stdout
+
+
+def tensor_rule_to_strings(
     interpretation: Dict[str, np.ndarray], rule: Dict[str, np.ndarray]
-) -> np.ndarray:
-    """Generates logic program files and runs them with clingo to check satisfiability."""
+) -> List[str]:
+    """Convert rule tensor with respect to a given interpretation into strings."""
     # interpretation {'nullary': (B, P0), 'unary': (B, O, P1), 'binary': (B, O, O-1, P2)}
     # rule {'and_kernel': (H, IN), 'or_kernel': (H,), 'num_variables': int}
     # ---------------------------
@@ -37,9 +56,6 @@ def clingo_check(
     # ['nullary(0)', 'unary(V0,0)', 'unary(V0,1)', 'unary(V1,0)',
     # 'unary(V1,1)', 'binary(V0,V1,0)', 'binary(V0,V1,1)', 'binary(V1,V0,0)',
     # 'binary(V1,V0,1)']
-    # ---------------------------
-    # target rule must be satisfied, to get satisfiable output
-    rule_lines: List[str] = [":- not t."]
     # ---------------------------
     # Construct the rule
     conjuncts: List[str] = list()
@@ -71,26 +87,36 @@ def clingo_check(
         for i in range(len(conjuncts))
         if rule["or_kernel"][i] != 0
     ]
-    rule_lines.extend(disjuncts)
     # ---------------------------
-    # That's the rule done, now add the interpretation
+    return disjuncts
+
+
+def tensor_interpretations_to_strings(
+    interpretation: Dict[str, np.ndarray]
+) -> List[List[str]]:
+    """Convert batched interpretations into an iterable list of strings."""
+    # interpretation {'nullary': (B, P0), 'unary': (B, O, P1), 'binary': (B, O, O-1, P2)}
+    # ---------------------------
     batch_size = interpretation["nullary"].shape[0]  # B
     num_objects = interpretation["unary"].shape[1]  # O
-    rule_lines.append(f"obj(0..{num_objects-1}).")
-    results: List[bool] = list()
-    for bidx in tqdm.trange(batch_size):
-        logic_program = rule_lines.copy()
+    obj_bidxs = np.stack(np.nonzero(1 - np.eye(num_objects))).T  # (O*(O-1), 2)
+    num_nullary = interpretation["nullary"].shape[-1]
+    num_unary = interpretation["unary"].shape[-1]
+    num_binary = interpretation["binary"].shape[-1]
+    # ---------------------------
+    programs: List[List[str]] = list()
+    for bidx in range(batch_size):
         nullary = interpretation["nullary"][bidx]  # (P0,)
         unary = interpretation["unary"][bidx]  # (O, P1)
         binary = interpretation["binary"][bidx]  # (O, O-1, P2)
         # ---------------------------
         # Add nullary facts.
-        logic_program.extend(
-            [f"nullary({i})." if nullary[i] == 1 else "" for i in range(num_nullary)]
-        )
+        program = [
+            f"nullary({i})." if nullary[i] == 1 else "" for i in range(num_nullary)
+        ]
         # ---------------------------
         # Add unary facts.
-        logic_program.extend(
+        program.extend(
             [
                 f"unary({i},{j})." if unary[i, j] == 1 else ""
                 for i, j in itertools.product(range(num_objects), range(num_unary))
@@ -98,26 +124,35 @@ def clingo_check(
         )
         # ---------------------------
         # Add binary facts.
-        obj_bidxs = np.stack(np.nonzero(1 - np.eye(num_objects))).T  # (O*(O-1), 2)
-        logic_program.extend(
+        program.extend(
             [
                 f"binary({i},{j},{k})." if binary[i, j - (j >= i), k] == 1 else ""
                 for (i, j), k in itertools.product(obj_bidxs, range(num_binary))
             ]
         )
         # ---------------------------
-        # Write the program file.
-        with open("clingo_temp.lp", "w") as fout:
-            fout.write("\n".join(logic_program))
-        # ---------------------------
-        # Ask clingo the result
-        # Clingo for some reason returns exit code 10 on success, we won't have return check
-        res = subprocess.run(
-            ["clingo", "clingo_temp.lp"], capture_output=True, check=False, text=True
-        )
-        results.append("UNSATISFIABLE" not in res.stdout)
+        programs.append(program)
+    return programs
+
+
+def clingo_check(
+    interpretation: Dict[str, np.ndarray], rule: Dict[str, np.ndarray]
+) -> np.ndarray:
+    """Generates logic program files and runs them with clingo to check satisfiability."""
+    # interpretation {'nullary': (B, P0), 'unary': (B, O, P1), 'binary': (B, O, O-1, P2)}
+    # rule {'and_kernel': (H, IN), 'or_kernel': (H,), 'num_variables': int}
     # ---------------------------
-    # Clean up
-    os.remove("clingo_temp.lp")
+    # target rule must be satisfied, to get satisfiable output
+    rule_lines: List[str] = [":- not t."]
+    rule_lines.extend(tensor_rule_to_strings(interpretation, rule))
+    # ---------------------------
+    # That's the rule done, now add the interpretation
+    num_objects = interpretation["unary"].shape[1]  # O
+    rule_lines.append(f"obj(0..{num_objects-1}).")
+    results: List[bool] = list()
+    for ground_interpretation in tqdm.tqdm(
+        tensor_interpretations_to_strings(interpretation)
+    ):
+        results.append(run_clingo(rule_lines + ground_interpretation))
     # ---------------------------
     return np.array(results)  # (B,)
