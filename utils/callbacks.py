786e8af93fddf6b521f7ee806f28635c7315f38b
nuric <nuric@users.noreply.github.com>
2021-04-23 14:42:04 +0100

    Add extra thresholding heuristics

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -9,6 +9,7 @@ import socket
 import numpy as np
 import tensorflow as tf
 import mlflow
+from sklearn.cluster import KMeans
 
 from reportlib import create_report
 from components.dnf_layer import WeightedDNF
@@ -340,7 +341,7 @@ class DNFPruner(tf.keras.callbacks.Callback):
         weight.assign(curr_weight)
         return prune_count
 
-    def threshold_weight(
+    def old_threshold_weight(
         self, weight: tf.Variable, threshold: float = 2.01, constant_value: float = 1.0
     ):
         """Threshold given weight by setting it to the given constant value."""
@@ -351,6 +352,72 @@ class DNFPruner(tf.keras.callbacks.Callback):
         neg_mask = tf.cast(weight < -threshold, tf.float32)
         new_weight = pos_mask * constant_value - neg_mask * constant_value
         # ---------------------------
+        # Assign the new weight
+        weight.assign(new_weight)
+
+    def threshold_weight(self, weight: tf.Variable, constant_value: float = 1.0):
+        """Threshold given weight by setting it to the given constant value."""
+        # weight (...)
+        # We will set positive entries to constant_value and
+        # negative values to -constant value
+        np_weight = weight.read_value().numpy()
+        # Cluster if the atom is in the rule or not, 2 clusters.
+        if np.any(np_weight == 0):
+            # Something has been pruned so we have 2 clusters
+            clustering = KMeans(2)
+            flat_weight = np_weight.flatten()[:, None]  # (W, 1)
+            clusters = clustering.fit_predict(np.abs(flat_weight))  # (W,)
+            clusters = clusters.reshape(np_weight.shape)  # (...)
+            # Figure which cluster is which
+            if clustering.cluster_centers_[0, 0] > clustering.cluster_centers_[1, 0]:
+                clusters = 1 - clusters
+        else:
+            # Nothing has been pruned, we will threshold everything
+            clusters = np.ones_like(np_weight, dtype=np.float32)
+        new_weight = clusters * tf.sign(weight) * constant_value
+        # ---------------------------
+        # Assign the new weight
+        weight.assign(new_weight)
+
+    def sample_threshold_weight(self, weight: tf.Variable, constant_value: float = 1.0):
+        """Threshold given weight by setting it to the given constant value."""
+        # weight (...)
+        # We will sample based on the weights, check their performance
+        # ---------------------------
+        # Get the validation dataset
+        vdset = next(v for k, v in self.datasets.items() if k.startswith("validation"))
+        # ---------------------------
+        # Collect samples based on the weight
+        num_tries = 1000
+        np_weight = tf.nn.tanh(weight.read_value()).numpy()
+        rng = np.random.default_rng()
+        samples = rng.random(num_tries * np_weight.size, dtype=np.float32)  # (N*W,)
+        samples = samples.reshape((num_tries, np_weight.size))  # (N, W)
+        abs_weight = np.abs(np_weight)  # (N,)
+        samples = samples < abs_weight.flatten() / abs_weight.max()  # (N, W)
+        samples = np.unique(samples, axis=0)  # (<N, W)
+        samples = samples.reshape(samples.shape[:1] + np_weight.shape)  # (N, ...)
+        # ---------------------------
+        # Evaluate samples
+        sample_scores: List[float] = list()
+        for sample in samples:
+            weight.assign(sample)
+            # ---------------------------
+            test_log: Dict[str, float] = self.model.evaluate(
+                vdset, verbose=0, return_dict=True
+            )
+            # {'acc': 0.5, 'loss': 0...}
+            # or {'label_acc':}
+            acc_key = "loss" if "loss" in test_log else "label_loss"
+            # ---------------------------
+            sample_scores.append(test_log[acc_key])
+        # ---------------------------
+        # Find the best sampled weight
+        best_idx = np.array(sample_scores).argmin()
+        best_sample = samples[best_idx]  # (...,)
+        # ---------------------------
+        # Threshold based on best sample
+        new_weight = best_sample * tf.sign(weight) * constant_value
         # Assign the new weight
         weight.assign(new_weight)
 
